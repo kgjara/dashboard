b668feca166431a59b6dedf08e59f5f6b4ccc2be
Kenny Jara <kgjara@espol.edu.ec>
2024-06-19 09:34:32 -0500

    indicator

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 
 import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
 import './App.css'
+import Indicator from './components/Indicator';
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <Grid xs={12} sm={4} md={3} lg={2}>4</Grid>
       <Grid xs={6} sm={4} md={6} lg={2}>5</Grid>
       <Grid xs={6} sm={4} md={6} lg={2}>6</Grid>
+      
     </Grid>
   )
 }
