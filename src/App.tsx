927872dda31abc1590eabd4c9ec74f0c17e9b646
Kenny Jara <kgjara@espol.edu.ec>
2024-06-24 10:56:32 -0500

    cambios

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,38 @@ import './App.css'
 import Indicator from './components/Indicator';
 import Summary from './components/Summary';
 import BasicTable from './components/BasicTable';
-
+import WeatherChart from './components/WeatherChart';
+import ControlPanel from './components/ControlPanel';
 
 function App() {
 
   return (
     <Grid container spacing={5}>
-      <Grid xs={12} sm={4} md={3} lg={2}>1</Grid>
-      <Summary></Summary>
-      <Grid xs={6} sm={4} md={3} lg={2}>2</Grid>
-      <BasicTable />
-      <Grid xs={6} sm={4} md={3} lg={2}>3</Grid>
-      <Grid xs={12} sm={4} md={3} lg={2}>4</Grid>
-      <Grid xs={6} sm={4} md={6} lg={2}>5</Grid>
-      <Grid xs={6} sm={4} md={6} lg={2}>6</Grid>
+      <Grid xs={12} sm={4} md={3} lg={12}>
+        <Summary></Summary>
+      </Grid>
+      <Grid xs={6} sm={4} md={3} lg={2}>
         <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
+      </Grid>
+      <Grid xs={6} sm={4} md={3} lg={2}>
+        <ControlPanel />
+      </Grid>
+      <Grid xs={12} sm={4} md={3} lg={2}></Grid>
+      <Grid xs={6} sm={4} md={6} lg={2}></Grid>
+      <Grid xs={4} sm={12} md={12} lg={12}>
+        <WeatherChart></WeatherChart>
+      </Grid>
+      <Grid lg={6}>
+        <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
+      </Grid>
+      <Grid lg={6}>
+        <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
+      </Grid>
+      <Grid xs={4} sm={12} md={12} lg={12}>
+        <Grid lg={9}>
+          <BasicTable />
+        </Grid>
+      </Grid>
     </Grid>
   )
 }
