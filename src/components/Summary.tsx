3191e0ca20b381d1423a85a922489ce3162ea499
Kenny Jara <84399723+kgjara@users.noreply.github.com>
2024-07-16 00:13:39 -0500

    summary cambio

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import Typography from '@mui/material/Typography';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
@@ -5,26 +6,60 @@ import CardMedia from '@mui/material/CardMedia';
 import { CardActionArea } from '@mui/material';
 
 import sunrise from '../assets/newSunrise.jpg'
+import sunset from '../assets/newSunset.jpg';
+import night from '../assets/newNight.jpg';
 
 export default function Summary() {
+
+    const [currentTime, setCurrentTime] = useState(new Date());
+    const [image, setImage] = useState(sunrise);
+
+    useEffect(() => {
+        // Update the time every second
+        const timer = setInterval(() => {
+            setCurrentTime(new Date());
+        }, 1000);
+
+        // Cleanup the interval on component unmount
+        return () => clearInterval(timer);
+    }, []);
+
+    useEffect(() => {
+        const hours = currentTime.getHours();
+        if (hours >= 6 && hours < 18) {
+            setImage(sunrise);
+        } else if (hours >= 18 && hours < 20) {
+            setImage(sunset);
+        } else {
+            setImage(night);
+        }
+    }, [currentTime]);
+
+    const formatTime = date => {
+        return date.toLocaleTimeString('es-ES', { hour: '2-digit', minute: '2-digit', second: '2-digit' });
+    };
+
+    const formatDate = date => {
+        return date.toLocaleDateString('es-ES', { year: 'numeric', month: 'long', day: 'numeric' });
+    };
     return (
         <Card sx={{ width: '100%' }}>
             <CardActionArea>
                 <CardMedia
                     component="img"
                     height="140"
-                    image={sunrise}
-                    alt="Amanecer"
+                    image={image}
+                    alt="Tiempo del día"
                 />
                 <CardContent>
                     <Typography gutterBottom component="h2" variant="h6" color="primary">
-                        Amanecer
+                        {currentTime.getHours() >= 6 && currentTime.getHours() < 18 ? 'Amanecer' : currentTime.getHours() >= 18 && currentTime.getHours() < 20 ? 'Atardecer' : 'Noche'}
                     </Typography>
                     <Typography component="p" variant="h4">
-                        05:19:08
+                        {formatTime(currentTime)}
                     </Typography>
                     <Typography color="text.secondary" sx={{ flex: 1 }}>
-                    	en 17 Junio, 2024
+                        {formatDate(currentTime)}
                     </Typography>
                 </CardContent>
             </CardActionArea>
